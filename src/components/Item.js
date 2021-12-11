f9b3bcf806a7cf576ca9773c085ce1c44b971aff
Moinak Banerjee <moinak878@gmail.com>
2021-12-11 08:01:11 +0530

    item qty on menu page

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,23 +1,27 @@
-import { React, useContext } from "react";
+import { React, useContext, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { CartContext } from "../Context";
 
 const Item = ({ menu }) => {
 	const { cart, addToCart, removeFromCart, decrement } =
 		useContext(CartContext);
-	
-
+	const [thisItem] = useState(menu);
 	return (
 		<div>
 			<Card className="d-flex m-3">
 				<Card.Body>
 					<Card.Title>{menu.name}</Card.Title>
 					<Card.Text>{menu.price}</Card.Text>
-					<h1>{menu.qty}</h1>
-					<Button variant="primary" onClick={() => addToCart(menu)}>
+
+					<Button variant="outline-primary" onClick={() => addToCart(menu)}>
 						+
 					</Button>
-					<Button variant="primary" onClick={() => decrement(menu)}>
+					{cart.map((menu) => (
+						<span style={{ margin: 10 }}>
+							{menu.name === thisItem.name ? menu.qty : null}
+						</span>
+					))}
+					<Button variant="outline-primary" onClick={() => decrement(menu)}>
 						-
 					</Button>
 					{cart.some((e) => e.name === menu.name && e.id === menu.id) && (
