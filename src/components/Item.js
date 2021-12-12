225414b1bd0de6ee4f269aa9e6e0a4071283261e
Moinak Banerjee <moinak878@gmail.com>
2021-12-12 21:24:18 +0530

    menu ui v1

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,37 +1,58 @@
 import { React, useContext, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { CartContext } from "../Context";
-
+import { FaPlus, FaMinus, FaTrash } from "react-icons/fa";
 const Item = ({ menu }) => {
 	const { cart, addToCart, removeFromCart, decrement } =
 		useContext(CartContext);
 	const [thisItem] = useState(menu);
 	return (
 		<div>
-			<Card className="d-flex m-3">
-				<Card.Body>
-					<Card.Title>{menu.name}</Card.Title>
-					<Card.Text>{menu.price}</Card.Text>
+			<Card className=" d-flex m-5 my-4">
+				<Card.Body className="d-flex ">
+					<img
+						className=" rounded"
+						style={{ width: "200px", height: "150px" }}
+						src="https://b.zmtcdn.com/data/dish_photos/cdb/d77c370349c174e0ff22a35c072decdb.jpg"
+						alt="food pic"
+					/>
+					<div className="d-flex flex-column">
+						<Card.Title className="font-weight-bold m-3">
+							{menu.name}
+						</Card.Title>
+						<Card.Text className="mx-3">Rs. {menu.price}0</Card.Text>
+					</div>
 
-					<Button variant="outline-primary" onClick={() => addToCart(menu)}>
-						+
-					</Button>
-					{cart.map((menu) => (
-						<span style={{ margin: 10 }}>
-							{menu.name === thisItem.name ? menu.qty : null}
-						</span>
-					))}
-					<Button variant="outline-primary" onClick={() => decrement(menu)}>
-						-
-					</Button>
-					{cart.some((e) => e.name === menu.name && e.id === menu.id) && (
-						<Button
-							variant="primary bg-warning m-3"
-							onClick={() => removeFromCart(menu)}
-						>
-							Remove from cart
-						</Button>
-					)}
+					<div className="hei">
+						<div className="d-flex ">
+							<Button
+								variant="outline-success"
+								style={{ margin: "5px" }}
+								onClick={() => addToCart(menu)}
+							>
+								<FaPlus className="m-1" />
+							</Button>
+
+							<Button
+								variant="outline-danger h-2"
+								style={{ margin: "5px" }}
+								onClick={() => decrement(menu)}
+							>
+								<FaMinus className="m-1" />
+							</Button>
+						</div>
+						<div className=" d-flex">
+							{cart.some((e) => e.name === menu.name && e.id === menu.id) && (
+								<Button
+									style={{ height: "40px", width: "60px", margin: 1 }}
+									variant="outline-secondary  m-3"
+									onClick={() => removeFromCart(menu)}
+								>
+									<FaTrash />
+								</Button>
+							)}
+						</div>
+					</div>
 				</Card.Body>
 			</Card>
 		</div>
@@ -39,3 +60,9 @@ const Item = ({ menu }) => {
 };
 
 export default Item;
+
+// {cart.map((menu) => (
+// 								<span style={{ margin: 10 }}>
+// 									{menu.name === thisItem.name ? menu.qty : null}
+// 								</span>
+// 							))}
